244ca6086b251dc1dc75c1f576feaec8338f5dbc
justalk <justal.kevin@gmail.com>
2022-11-28 08:38:54 +0100

    [FEATURE] Cleaning exp

--- react/apps/app/src/app/views/Experience8/Inside.tsx
+++ react/apps/app/src/app/views/Experience8/Inside.tsx
@@ -1,20 +1,8 @@
-import { useState, useEffect } from 'react';
-import { Container, BitmapText } from 'react-pixi-fiber';
-import * as PIXI from 'pixi.js';
+import { Container } from 'react-pixi-fiber';
 import Rectangle from './components/Rectangle';
 import Circle from './components/Circle';
 
 export function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    PIXI.Assets.load(
-      'https://pixijs.io/examples/examples/assets/bitmap-font/desyrel.xml'
-    ).then(() => {
-      setIsLoaded(true);
-    });
-  }, []);
-
   return (
     <Container>
       <Rectangle x={50} y={50} width={100} height={100} fill={0xffff00} />
